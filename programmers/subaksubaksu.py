a811207ccf4b4dbeb7682d2fd28c9dcd4728e80e
daking <48229706+daking0@users.noreply.github.com>
2022-08-02 09:18:08 +0900

    프로그래머스 수박수박수박수박수박수?

---
+++ programmers/subaksubaksu.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12922
+def solution(n):
+    answer = ''
+    if(n%2 == 0):
+        answer = '수박'*(n//2)
+    else:
+        answer = '수박'*(n//2) +'수'
+    return answer
